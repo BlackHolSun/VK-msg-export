a7a93132c7125b7a2fc330d09e06295fbb885e98
BlackHoleSun <imladyartist@gmail.com>
2019-02-04 19:22:02 -0500

    added unit test

--- src/test/java/com/imladyartist/AppTest.java
+++ src/test/java/com/imladyartist/AppTest.java
@@ -9,12 +9,14 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( new TokenReq().reqUrl.equals("https://oauth.vk.com/authorize" + "?client_id=6457262"
+                + "&scope=messages"
+                + "&redirect_uri=https://oauth.vk.com/blank.html"
+                + "&response_type=token"
+                + "&v=5.74") );
     }
 }
